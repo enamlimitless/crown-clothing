f62550c1385a1493debe642a359c9f64bb61ca5a
enamulhaqe7 <enamulhaquemb01@gmail.com>
2022-01-04 01:22:37 +0600

    updating menu-item

---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MenuItem = ({section}) => {
+    const {title, imageUrl, size} = section;
+    return (
+            <div style={{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
+                <div className="menu-content">
+                    <h1 className='title'>{title}</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+    );
+};
+
+export default MenuItem;
